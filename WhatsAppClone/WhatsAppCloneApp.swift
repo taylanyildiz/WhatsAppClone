4cbfb1a97ea98ea23fb75be8e7ed8380c87ddfc4
taylanyildiz <taylanyildz@outlook.com>
2024-06-21 08:31:29 +0300

    Initial Commit

---
+++ WhatsAppClone/WhatsAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppCloneApp.swift
+//  WhatsAppClone
+//
+//  Created by Developer on 21.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
